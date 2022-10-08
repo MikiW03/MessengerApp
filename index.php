e96568358a32fea279d2780dfce92a1617c559dc
MikiW03 <mikiwiniecki@gmail.com>
2022-10-08 22:34:20 +0200

    Prepare to host

--- index.php
+++ index.php
@@ -18,6 +18,9 @@
     $receiver = "John Doe";
 
     session_start();
+    print(!isset($_SESSION));
+    print(!isset($_SESSION['sender']));
+    print(!isset($_SESSION['receiver']));
     if (!isset($_SESSION) && (!isset($_SESSION['sender']) || !isset($_SESSION['receiver']))) {
         print("session not set");
         header('Location:' . "login.php");
